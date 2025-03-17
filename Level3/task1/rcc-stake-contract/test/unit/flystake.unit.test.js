a2c6b0f654b3950719947203e7b0201722633c28
lpf <boysunshiny@outlook.com>
2025-03-17 23:42:11 +0800

    11

--- Level3/task1/rcc-stake-contract/test/unit/flystake.unit.test.js
+++ Level3/task1/rcc-stake-contract/test/unit/flystake.unit.test.js
@@ -1,4 +1,4 @@
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 const { ethers, network, ignition } = require("hardhat");
 const { developmentChains } = require("../../scripts/config");
 const FLYStakeModule = require("../../ignition/modules/fly_stake");
@@ -11,17 +11,21 @@ const FLYStakeModule = require("../../ignition/modules/fly_stake");
         beforeEach(async () => {
             const accounts = await ethers.getSigners();
             deployer = accounts[0];
-            const deployment = await ignition.deploy(FLYStakeModule);
+            const module = await ignition.deploy(FLYStakeModule);
             contract = await ethers.getContractAt(
                 "FLYStake",
-                deployment.stakeProxy.target,
+                module.stakeProxy.target,
                 deployer
             );
         });
-
-        it("test for poolLength", async function () {
-            const s = await contract.poolLength();
-            assert.equal(s, "0");
+        //poolLength
+        describe("depositETH", function () {
+            it("直接调用depositETH会失败", async function () {
+                await expect(contract.depositETH()).to.be.reverted;
+            });
+            it("存入ETH到合约中", async function () {
+                const amount = ethers.parseEther("1");
+                await expect(contract.depositETH({ value: amount })).to.not.reverted;
+            });
         });
-
     });
