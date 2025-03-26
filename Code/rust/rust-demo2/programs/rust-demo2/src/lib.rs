95f3ec9fd4658a89e34faeb12f31658cd6c9e440
lpf <boysunshiny@outlook.com>
2025-03-26 17:05:22 +0800

    122

--- Code/rust/rust-demo2/programs/rust-demo2/src/lib.rs
+++ Code/rust/rust-demo2/programs/rust-demo2/src/lib.rs
@@ -1,11 +1,13 @@
 use anchor_lang::prelude::*;
 
-declare_id!("8tgECJCDaS747gMmRFCJkAdHZZbq5BA7tBcHmGUrrXcZ");
+declare_id!("GvBrcFw27V61tUgGrvr9tnp1Ajbt3ETofsm949MkUBFR");
 
 pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
 
 #[program]
 pub mod rust_demo2 {
+    // use futures::executor::block_on;
+
     use super::*;
     pub fn set_favorites(
         context: Context<SetFavorites>,
@@ -13,11 +15,16 @@ pub mod rust_demo2 {
         color: String,
         hobbies: Vec<String>,
     ) -> Result<()> {
+        // let b = async {
+        //     msg!("这是我的测试");
+        // };
+        // block_on(b);
+
         msg!("欢迎   {}", context.program_id);
         let user_public_key = context.accounts.user.key();
-        msg!(
-            "用户 {user_public_key} ,favorite number is {number} ,favorite color is {color}, favorite hobbies is {hobbies:#?}"
-        );
+        // msg!(
+        //     "用户 {user_public_key} ,favorite number is {number} ,favorite color is {color}, favorite hobbies is {hobbies:#?}"
+        // );
         msg!(
             "用户 {} ,favorite number is {} ,favorite color is {}, favorite hobbies is {:#?}",
             user_public_key,
@@ -30,6 +37,26 @@ pub mod rust_demo2 {
             color,
             hobbies,
         });
+        Ok(())
+    }
+
+    pub fn set_number(context: Context<SetFavorites>, number: u64) -> Result<()> {
+        msg!("欢迎 {}", context.program_id);
+        msg!("你爱的   {}", number);
+        Ok(())
+    }
+    pub fn set_number_and_color(
+        context: Context<SetFavorites>,
+        number: u64,
+        color: String,
+    ) -> Result<()> {
+        msg!("欢迎 {}", context.program_id);
+        msg!("你爱的   {}  喜欢的颜色 {}", number, color);
+        Ok(())
+    }
+    pub fn set_color(context: Context<SetFavorites>, color: String) -> Result<()> {
+        msg!("欢迎 {}", context.program_id);
+        msg!("你爱的   喜欢的颜色 {}", color);
         Ok(())
     }
 }
@@ -48,13 +75,20 @@ pub struct Favorites {
 pub struct SetFavorites<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
+    /*
+    init_if_needed,
+    payer = user,
+    space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+    seeds = [b"favorites",user.key().as_ref()],
+    bump
+    */
     #[account(
         init_if_needed,
         payer = user,
         space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
         seeds = [b"favorites",user.key().as_ref()],
         bump
-    )]
+        )]
     pub favorites: Account<'info, Favorites>,
     pub system_program: Program<'info, System>,
 }
--- Code/rust/rust-demo2/tests/rust-demo2.ts
+++ Code/rust/rust-demo2/tests/rust-demo2.ts
@@ -1,16 +1,44 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { RustDemo2 } from "../target/types/rust_demo2";
+import { BN } from "bn.js";
+import { Keypair, PublicKey, SystemProgram } from "@solana/web3.js";
+import { homedir } from "os";
+import { join } from "path";
+import { readFileSync } from "fs";
 
 describe("rust-demo2", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
   const program = anchor.workspace.RustDemo2 as Program<RustDemo2>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  const solanaConfigPath = join(homedir(), ".config", "solana", "id.json");
+  const secretKey = Uint8Array.from(
+    JSON.parse(readFileSync(solanaConfigPath, "utf-8"))
+  );
+  const user = Keypair.fromSecretKey(secretKey);
+
+  it("set favorites!", async () => {
+    const [PDA] = PublicKey.findProgramAddressSync(
+      [Buffer.from("favorites"), user.publicKey.toBuffer()],
+      program.programId
+    );
+    console.log("user address             ", user.publicKey);
+    console.log("program address          ", program.programId);
+    console.log("PDA address              ", PDA);
+    console.log("SystemProgram address    ", SystemProgram.programId);
+
+    const accounts = {
+      user: user.publicKey,
+      favorites: PDA,
+      systemProgram: SystemProgram.programId, // 注意这里是systemProgram不是system_program
+    };
+
+    console.log("accounts                 ", accounts);
+    const tx = await program.methods
+      .setFavorites(new BN(6), "red", ["read", "walk", "sing"])
+      .accounts(accounts)
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
